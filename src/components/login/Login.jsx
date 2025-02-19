91e2b6afc67cefec48b5d362b5d2d1d5cc608589
srmigjr <green25mig@gmail.com>
2025-02-19 19:48:18 -0300

    segundo commit

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,27 +1,38 @@
-import capa from "../../assets/Tela Login/imagem tela de login.png"
-import logo from "../../assets/Tela Login/logo-ws.png"
-function login(){
-    return(
+import { useNavigate } from "react-router-dom";
+import Logo from "../../assets/Tela Login/logo-ws.png";
+import styles from "./Login.module.scss";
 
-        <main>
-            <section>
-<p> imagem</p>
-            </section>
-            <section>
-            <img src={Logo} alt="Logo da Wilson sons" />
-<h1>Boas vindas ao Novo Porta SISPAR</h1>
-<p>Sistema de Emissao de boletos e parcelamento </p>
-<form action="">
-    <input type="text" />
-    <input type="text" />
-</form>
-            </section>
+function Login() {
+  const navigate = useNavigate(); //Iniciando o hook useNavigate
 
+  const irParaReembolsos = () => {
+    navigate("/reembolsos");
+  };
 
+  return (
+    <main>
+      <section className={styles.containerImagem}></section>
 
-            </main >
-            );
+      <section className={styles.containerForm}>
+        <img src={Logo} alt="Logo da Wilson sons" />
+        <h1>Boas vindas ao Novo Portal SISPAR</h1>
+        <p>Sistema de Emissão de Boletos e Parcelamento</p>
 
+        <form action="">
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <input type="password" name="senha" id="senha" placeholder="Senha" />
+
+          <a href="">Esqueci minha senha</a>
+
+          <div>
+            <button onClick={irParaReembolsos} className={styles.buttonEntrar}>  
+              Entrar
+            </button>
+            <button className={styles.buttonCriar}>Criar conta</button>
+          </div>
+        </form>
+      </section>
+    </main>
+  );
 }
-
-export default login;
+export default Login;
---
+++ src/components/solicitacao/Solicitacao.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../navbar/NavBar.jsx"
+
+function Solicitacao(){
+    return(
+        <>
+       <NavBar />
+        </>
+    )
+}
+export default Solicitacao
